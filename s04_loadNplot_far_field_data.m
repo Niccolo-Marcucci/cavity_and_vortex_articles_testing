d1304b57eebb8428260c3c8d61fcea89f4e9a775
Niccolo-Marcucci <niccolo.marcucci@polito.it>
2020-01-27 17:28:08 +0100

    work on far field data

--- s04_loadNplot_far_field_data.m
+++ s04_loadNplot_far_field_data.m
@@ -1,5 +1,9 @@
-clear, close all
-load far_field_data
+clear,close all
+folder="SIM02_no_cavity_spiral_outcoupler/sweep_charge_and_ngrating/far_field_data/";
+top_charge=2;
+for n_g=[10]
+ 
+load(strcat(folder,"far_field_data","_charge",string(top_charge),"_N",string(n_g)));
 % 
 % useful_ux = abs(ux)<0.2;
 % useful_uy = abs(uy)<0.2;
@@ -11,49 +15,52 @@ load far_field_data
 % E_theta = E_theta(useful_ux,useful_uy);
 
 
-% cos(theta) = uz
+% cos(thneta) = uz
 theta = real( acos( sqrt(1 - ux.^2 - uy.^2)));
 cos_phi = ux./sin(theta);
 sin_phi = uy./sin(theta);
 
-Ex = E_theta.*cos_phi + E_phi.*sin_phi;
+Ex = E_theta.*cos_phi - E_phi.*sin_phi;
 Ey = E_theta.*sin_phi + E_phi.*cos_phi;
 
 EL = +sqrt(2)/2*Ex + sqrt(2)/2*Ey*exp(+1i*pi/2);
 ER = -sqrt(2)/2*Ex + sqrt(2)/2*Ey*exp(+1i*pi/2);
 
-
-% figure
-% plot_surf(ux,uy,real(E_phi),"something about e_phi");
-% plot_surf(ux,uy,real(E_theta),"something about e_theta");
-% plot_surf(ux,uy,abs(Ey).^2   +abs(Ex).^2,'intensity from Ex and Ey');
-% plot_surf(ux,uy,abs(E_phi).^2+abs(E_theta).^2,'intensity from E_phi and E_theta');
-
-figure
-subplot(2,2,1)
-plot_surf(ux,uy,abs(ER).^2,"Right circular polarization intensity");
-subplot(2,2,2)
-plot_surf(ux,uy,abs(EL).^2,"Left circular polarization intensity");
-subplot(2,2,3)
-plot_surf(ux,uy,angle(ER),"Right circular polarization phase");
-subplot(2,2,4)
-plot_surf(ux,uy,angle(EL),"Left circular polarization phase");
-
-%%
 S3 = 1i*(Ex.*conj(Ey)-Ey.*conj(Ex));
 chi = 0.5*asin( real(S3)./(abs(Ex).^2+abs(Ey).^2));
 
+% figure
+% plot_surf(ux,uy,sin_phi,"something about e_phi");
+% plot_surf(ux,uy,theta,"something about e_theta");
+
 figure
-subplot(1,2,1)
-plot_surf(ux,uy,real(S3),"S3 Stokes parameter");
 subplot(1,2,2)
-plot_surf(ux,uy,abs(tan(chi)),"eccentricity |tan\chi|");
-    
+plot_surf(ux,uy,abs(Ey).^2   +abs(Ex).^2,'Intensity from Ex and Ey');
+subplot(1,2,1)
+plot_surf(ux,uy,abs(E_phi).^2+abs(E_theta).^2,'Intensity from E_\phi and E_\theta');
+%%
+% figure
+% subplot(2,3,1)
+% plot_surf(ux,uy,abs(ER).^2,"Right circular polarization intensity");
+% subplot(2,3,2)
+% plot_surf(ux,uy,abs(EL).^2,"Left circular polarization intensity");
+% subplot(2,3,4)
+% plot_surf(ux,uy,angle(ER),"Right circular polarization phase");
+% subplot(2,3,5)
+% plot_surf(ux,uy,angle(EL),"Left circular polarization phase");
+% 
+% subplot(2,3,3)
+% plot_surf(ux,uy,real(S3),"S3 Stokes parameter");
+% subplot(2,3,6)
+% plot_surf(ux,uy,abs(tan(chi)),"eccentricity |tan\chi|");
+end
+
+
 
 function plot_surf(ux,uy,Quantity,picture_title)
     s=surf(ux,uy,Quantity);
     s.EdgeColor='none';
-    colormap('jet')
+    colormap('hot')
     colorbar
     view(2)
     xlabel("ux");
