e30031de8cc3d6014d7f3a8550239c41a35b1d9d
Niccolo-Marcucci <niccolo.marcucci@polito.it>
2020-02-11 17:35:26 +0100

    just working on different names

--- s04_loadNplot_far_field_data.m
+++ s04_loadNplot_far_field_data.m
@@ -1,9 +1,9 @@
 clear,close all
 folder="SIM02_spiral_outcoupler/far_field_data/";
-
+%betterSource_charge
 for top_charge=1
  
-load(strcat(folder,"spiral_outcoupler","betterSource_charge",string(top_charge)));
+load(strcat(folder,"spiral_outcoupler","CloserSource_charge",string(top_charge)));
 % 
 % useful_ux = abs(ux)<0.2;
 % useful_uy = abs(uy)<0.2;
