62574c401ccc59607f343e86cd792776bee8edf5
Niccolo-Marcucci <niccolo.marcucci@polito.it>
2020-02-17 17:54:26 +0100

    Added possibility for far field avaraging

--- s04_loadNplot_far_field_data.m
+++ s04_loadNplot_far_field_data.m
@@ -1,27 +1,36 @@
-clear,close all
+clear%,close all
 folder="SIM02_spiral_outcoupler/far_field_data/";
 %betterSource_charge
 for top_charge=1
- 
-load(strcat(folder,"spiral_outcoupler","CloserSource_charge",string(top_charge)));
-% 
+load(strcat(folder,"spiral_outcoupler","_charge",string(top_charge),"_539"));
+% load(strcat(folder,"SPIRALE_PEC_nonaz"))CloserSource_smalleRadius
 % useful_ux = abs(ux)<0.2;
 % useful_uy = abs(uy)<0.2;
-
-[ux,uy]=meshgrid(ux,uy);
 % ux = ux(useful_ux,useful_uy);
 % uy = uy(useful_ux,useful_uy);
 % E_phi = E_phi(useful_ux,useful_uy);
 % E_theta = E_theta(useful_ux,useful_uy);
 
+E_theta2 = zeros(size(E_phi));
+E_phi2 = E_theta2;
+ for lambda = [ 524,531, 539]
+load(strcat(folder,"spiral_outcoupler","_charge",string(top_charge),"_",string(lambda)));
+E_theta2 = E_theta2 + E_theta;
+E_phi2 = E_phi2 + E_phi;
+ end
 
+ E_theta=E_theta2/3;
+ E_phi=E_phi2/3;
+ 
+ [ux,uy]=meshgrid(ux,uy);
+ 
 % cos(thneta) = uz
 theta = real( acos( sqrt(1 - ux.^2 - uy.^2)));
 cos_phi = ux./sin(theta);
 sin_phi = uy./sin(theta);
 
-Ex = E_theta.*cos_phi - E_phi.*sin_phi;
-Ey = E_theta.*sin_phi + E_phi.*cos_phi;
+Ex = + E_phi.*sin_phi + E_theta.*cos_phi;%
+Ey = - E_phi.*cos_phi + E_theta.*sin_phi;%
 
 EL = sqrt(2)/2*( Ex + Ey*exp(+1i*pi/2));
 ER = sqrt(2)/2*(-Ex + Ey*exp(+1i*pi/2));
@@ -33,12 +42,12 @@ chi = 0.5*asin( real(S3)./(abs(Ex).^2+abs(Ey).^2));
 % plot_surf(ux,uy,sin_phi,"something about e_phi");
 % plot_surf(ux,uy,theta,"something about e_theta");
 
-figure
-subplot(1,2,2)
-plot_surf(ux,uy,abs(Ey).^2   +abs(Ex).^2,'Intensity from Ex and Ey');
-subplot(1,2,1)
-plot_surf(ux,uy,abs(E_phi).^2+abs(E_theta).^2,'Intensity from E_\phi and E_\theta');
-%%
+% figure
+% subplot(1,2,2)
+% plot_surf(ux,uy,abs(Ey).^2   +abs(Ex).^2,'Intensity from Ex and Ey');
+% subplot(1,2,1)
+% plot_surf(ux,uy,abs(E_phi).^2 + abs(E_theta).^2,'Intensity from E_\phi and E_\theta');
+% %%
 figure
 subplot(2,3,1)
 plot_surf(ux,uy,abs(ER).^2,"Right circular polarization intensity");
@@ -53,6 +62,7 @@ subplot(2,3,3)
 plot_surf(ux,uy,real(S3),"S3 Stokes parameter");
 subplot(2,3,6)
 plot_surf(ux,uy,abs(tan(chi)),"eccentricity |tan\chi|");
+%  end
 end
 
 
